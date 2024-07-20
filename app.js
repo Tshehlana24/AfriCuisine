a66ae75c0057af8acd0913be52dcd5b10daabf3f
Tshehlana <tshehlanamonyebodi@gmail.com>
2024-07-20 08:05:01 +0200

    navgation

---
+++ app.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const tabsContainer = document.querySelector('.navbar');
+    if (window.scrollY > window.innerHeight) {
+        tabsContainer.classList.add('et-hero-tabs-container--top');
+    } else {
+        tabsContainer.classList.remove('et-hero-tabs-container--top');
+    }
+});
